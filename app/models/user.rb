6dba5c0721cd447701cac660b521eb94d95b4e38
heronmedeiros <heron.medeiros@gmail.com>
2016-01-30 19:12:25 -0300

    Fixed specs and validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ActiveRecord::Base
   has_many :tweets
 
-  validates :name,  presence: true, length: { maximum: 40 }
-  validates :email, presence: true, length: { maximum: 255 }
+  validates_uniqueness_of :email, on: :create
+
+  EMAIL_REGEX = /\A[\w+\-]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :name,  presence: true,
+                    length: { maximum: 40 },
+                    uniqueness: true
+  validates :email, presence: true,
+                    length: { maximum: 255 },
+                    format: { with: EMAIL_REGEX }
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,19 +3,43 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
 
   it { should have_many(:tweets) }
-  
+
   context "validations" do
-    before do
-      @user = User.new
+
+    it { should validate_length_of(:name).is_at_most(40) }
+    it { should validate_length_of(:email).is_at_most(255) }
+    it { should validate_uniqueness_of(:email).on(:create) }
+    it { should validate_uniqueness_of(:name) }
+    it { should have_secure_password }
+
+    context "when fields are empty" do
+      let (:user) { User.new }
+
+      it  "should not be valid" do
+        expect(user.valid?).to be_falsy
+      end
+
+      it "should not save" do
+        expect(user.save).to be_falsy
+      end
     end
 
-    it "expect to name is not to long" do
-      @user.name = "lee" * 51
-      expect(@user.valid?)
-    end
+    context "when fiels are not empty" do
+      let (:user) { User.new(
+                            name: "John Lee",
+                            email: "johnlee@email.com")
+                        }
 
-    xit "email addresses should be unique" do
-     end
+      it "should save" do
+        expect(user.save).to be_truthy
+      end
+
+      it "expect to name is not to long" do
+        user.name = "lee" * 69
+        expect(user.valid?).to be_falsy
+        expect(user.errors[:name]).to eq(["is too long (maximum is 40 characters)"])
+      end
+
+    end
   end
-
 end
